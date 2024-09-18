fdb61e21535ca18060c57ddb3801b2bbb423e959
Nazam-Kalsi <nzamkalsi@gmail.com>
2024-09-18 14:28:13 +0530

    add each reply as an individual row

--- get_data.py
+++ get_data.py
@@ -21,7 +21,6 @@ def setup():
     chrome_options.add_argument("--disable-dev-shm-usage")
     return webdriver.Chrome(options=chrome_options)
 
-
 def parse_urls(urls):
     data = {
         "url": [],
@@ -38,12 +37,12 @@ def parse_urls(urls):
         "labels_list": [],
         "number_of_views": [],
         "number_of_likes": [],
-        "replies": [],
         "accepted_sol": [],
+        "parent_post": [], 
     }
 
     for i, url in enumerate(urls):
-        if i==3:break;
+        # if i == 3: break
         print(f"Parsing url number : {i+1}")
         driver = setup()
         try:
@@ -52,85 +51,30 @@ def parse_urls(urls):
                 EC.presence_of_element_located((By.CSS_SELECTOR, "h1.page-title"))
             )
         except:
-            print("A pinned post.(from anouncements)")
+            print("A pinned post (from announcements).")
             continue
 
         try:
-            # Title
+            # Title and User Details
             title = driver.find_element(By.CSS_SELECTOR, "h1.page-title").text
-
-            # User Details
             user_name = driver.find_element(By.CSS_SELECTOR, "a.lia-user-name-link")
+            user_title = driver.find_element(By.CSS_SELECTOR, ".lia-message-author-rank").text.strip()
+            user_posts = driver.find_element(By.CSS_SELECTOR, ".lia-message-author-posts-count").text
+            solution_by_user = driver.find_element(By.CSS_SELECTOR, ".lia-message-author-solutions-count").text.strip()
+            user_kudos = driver.find_element(By.CSS_SELECTOR, ".lia-message-author-kudos-count").text.strip()
 
-            user_title = driver.find_element(
-                By.CSS_SELECTOR, ".lia-message-author-rank"
-            ).text.strip()
+            # Post Details
+            publish_date = driver.find_element(By.CSS_SELECTOR, ".DateTime > meta").get_attribute("content")
+            post_content = driver.find_element(By.CSS_SELECTOR, ".lia-message-body-content").text
+            solved = "Y" if driver.find_elements(By.CSS_SELECTOR, ".lia-panel-feedback-banner-safe") else "N"
+            # accepted_sol = " ".join([elem.text for elem in driver.find_elements(By.CSS_SELECTOR, ".lia-message-body-content")[1:]]) if solved == "Y" else ""
+            accepted_sol = ""
 
-            # User Stats
-            user_posts = driver.find_element(
-                By.CSS_SELECTOR, ".lia-message-author-posts-count"
-            ).text
+            labels = [label.text.strip() for label in driver.find_elements(By.CSS_SELECTOR, ".label")]
+            views = driver.find_element(By.CSS_SELECTOR, ".lia-message-stats-count").text.strip() if driver.find_elements(By.CSS_SELECTOR, ".lia-message-stats-count") else "0"
+            likes = driver.find_element(By.CSS_SELECTOR, ".MessageKudosCount").text.strip()
 
-            solution_by_user = driver.find_element(
-                By.CSS_SELECTOR, ".lia-message-author-solutions-count"
-            ).text.strip()
-            user_kudos = driver.find_element(
-                By.CSS_SELECTOR, ".lia-message-author-kudos-count"
-            ).text.strip()
-
-            # Timestamps,Post Content, Solved Status and accepted solution if solved
-            publish_date = driver.find_element(
-                By.CSS_SELECTOR, ".DateTime > meta"
-            ).get_attribute("content")
-
-            post_content = driver.find_elements(
-                By.CSS_SELECTOR, ".lia-message-body-content"
-            )[0]
-
-            solved = (
-                "Y"
-                if driver.find_elements(
-                    By.CSS_SELECTOR, ".lia-panel-feedback-banner-safe"
-                )
-                else "N"
-            )
-
-            accepted_sol = (
-                " ".join(
-                    [
-                        elem.text
-                        for elem in driver.find_elements(
-                            By.CSS_SELECTOR, ".lia-message-body-content"
-                        )[1:]
-                    ]
-                )
-                if solved == "Y"
-                else "NaN"
-            )
-
-            # Labels, Views, Likes, and Replies
-            labels = [
-                label.text.strip()
-                for label in driver.find_elements(By.CSS_SELECTOR, ".label")
-            ]
-
-            views = driver.find_element(By.CSS_SELECTOR, ".lia-message-stats-count")
-
-            likes = driver.find_element(By.CSS_SELECTOR, ".MessageKudosCount")
-
-            replies = driver.find_elements(
-                By.CSS_SELECTOR, ".lia-threaded-detail-display-message-view"
-            )
-            reply_texts = []
-            for reply in replies:
-                try:
-                    reply_content = reply.find_element(
-                        By.CSS_SELECTOR, "div.lia-message-body"
-                    )
-                    reply_texts.append(reply_content.text)
-                except:
-                    print("Nested div not found in one of the replies.")
-
+            # Store parent post data
             data["url"].append(url)
             data["title"].append(title)
             data["user_name"].append(user_name.text)
@@ -140,24 +84,66 @@ def parse_urls(urls):
             data["number_of_kudos_by_user"].append(user_kudos)
             data["number_of_solutions_by_user"].append(solution_by_user)
             data["timestamp"].append(publish_date)
-            data["post_content"].append(post_content.text)
+            data["post_content"].append(post_content)
             data["solved"].append(solved)
             data["accepted_sol"].append(accepted_sol)
             data["labels_list"].append(labels)
-            data["number_of_likes"].append(likes.text.strip())
-            data["number_of_views"].append(views.text.strip() if views else "0")
-            data["replies"].append(reply_texts)
+            data["number_of_likes"].append(likes)
+            data["number_of_views"].append(views)
+            data["parent_post"].append("Y")
+
+            try:
+                replies_container=driver.find_element(By.ID, "threadeddetailmessagelist")
+                replies = replies_container.find_elements(By.CSS_SELECTOR, "div.lia-threaded-detail-display-message-view")
+                
+                for reply in replies:
+                    try:
+                        reply_user_name = reply.find_element(By.CSS_SELECTOR, "a.lia-user-name-link").text
+                        reply_content = reply.find_element(By.CSS_SELECTOR, "div.lia-message-body").text
+                        reply_timestamp = reply.find_element(By.CSS_SELECTOR, ".DateTime > meta").get_attribute("content")                        
+                        reply_user_profile_link = reply.find_element(By.CSS_SELECTOR, "a.lia-user-name-link").get_attribute("href")
+                        reply_user_title = reply.find_element(By.CSS_SELECTOR,"div.lia-message-author-rank").text
+                        reply_user_posts = reply.find_element(By.CSS_SELECTOR,"span.lia-message-author-posts-count").text
+                        reply_user_kudos_count = reply.find_element(By.CSS_SELECTOR,"span.lia-message-author-kudos-count").text
+                        reply_user_sol_count = reply.find_element(By.CSS_SELECTOR,"span.lia-message-author-solutions-count").text
+                        reply_user_sol_likes = reply.find_element(By.CSS_SELECTOR,"span.lia-component-kudos-widget-message-kudos-count").text
+                        accepted_reply_or_not = "Y" if reply.find_elements(By.CSS_SELECTOR, "p.accepted-solution-text") else "N"
+                        
+                        # store each reply data
+                        data["url"].append(url)
+                        data["title"].append(f"Reply to {title}")
+                        data["user_name"].append(reply_user_name)
+                        data["user_profile_link"].append(reply_user_profile_link)  
+                        data["user_title"].append(reply_user_title)
+                        data["number_of_posts_by_user"].append(reply_user_posts)
+                        data["number_of_kudos_by_user"].append(reply_user_kudos_count)
+                        data["number_of_solutions_by_user"].append(reply_user_sol_count)
+                        data["timestamp"].append(reply_timestamp)
+                        data["post_content"].append(reply_content)
+                        data["solved"].append("")
+                        data["labels_list"].append("")
+                        data["number_of_likes"].append(reply_user_sol_likes)
+                        data["number_of_views"].append("")
+                        data["parent_post"].append("N")
+                        data["accepted_sol"].append(accepted_reply_or_not)
+                    except Exception as e:
+                        print("Error extracting reply details:", e)
+            except:
+                print("No replies found for this post")
 
         except Exception as e:
-            print(f"error while parsing {url}")
+            print(f"Error while parsing {url}: {e}")
             continue
-    driver.quit()
+
+        finally:
+            driver.quit()
+
     return pd.DataFrame(data)
 
 
 if __name__ == "__main__":
 
     df = parse_urls(urls)
-    df.to_csv("shopify_discussion_data.csv")
-    print("Parsing Successfull")
-    print(f"Total items in dataset : {len(df)}")
+    df.to_csv("shopify_discussion_data.csv", index=False)
+    print("Parsing Successful")
+    print(f"Total items in dataset: {len(df)}")
